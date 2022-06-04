70ec6b3da867a3281cc124a15cfe86207931b9d1
josephpristaly <67723201+josephpristaly@users.noreply.github.com>
2022-06-04 09:27:00 +0600

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -51,7 +51,7 @@ class Progress:
             [
                 [
                     InlineKeyboardButton(
-                        "Cancel 🚫",
+                        "Cancel",
                         callback_data=(
                             f"gUPcancel/{chat_id}/{mes_id}/{from_user}"
                         ).encode("UTF-8"),
@@ -62,7 +62,7 @@ class Progress:
         if self.is_cancelled:
             LOGGER.info("stopping ")
             await self._mess.edit(
-                f"😔 Cancelled/ERROR: `{ud_type}` ({humanbytes(total)})"
+                f"Cancelled/ERROR: `{ud_type}` ({humanbytes(total)})"
             )
             await self._client.stop_transmission()
 
@@ -82,7 +82,7 @@ class Progress:
                 ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
                 round(percentage, 2))
             #cpu = "{psutil.cpu_percent()}%"
-            tmp = progress + "\n**• Total 📀:**`〘{1}〙`\n**• Done ✓ :**` 〘{0}〙`\n**• Speed 🚀 :** `〘{2}〙`\n**• ETA ⏳ :**` 〘{3}〙`".format(
+            tmp = progress + "\n**• Total:**`〘{1}〙`\n**• Done ✓ :**` 〘{0}〙`\n**• Speed:** `〘{2}〙`\n**• ETA:**` 〘{3}〙`".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),
